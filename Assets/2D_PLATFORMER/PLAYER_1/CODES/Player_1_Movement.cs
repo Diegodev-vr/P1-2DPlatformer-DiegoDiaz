3c4eab23a0d7c99ad52397646a4fbce559253268
Diego Diaz <diazdiego00@hotmail.com>
2026-02-22 14:25:52 -0500

    Create Input Actions + Add Background and Player Sprites
    I started the Player_1_Movement Code (Not Finished) and create a totally new input system for it, also import the sprites for background and player, both of these elements are now in the scene, the player_1 has now RigidBody and a Box Collider 2D, Player_1 has now a new sorting layer on the sprite renderer called ("Player") same for Background ("Background"), finally background has a Box collider 2D for the floor

---
+++ Assets/2D_PLATFORMER/PLAYER_1/CODES/Player_1_Movement.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+// this let me use the new input system
+using UnityEngine.InputSystem;
+
+public class Player_1_Movement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
